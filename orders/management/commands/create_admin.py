b2bc203f059fa68ad4f41a5689250454bdd2242c
Désiré Tongbé <desire@example.com>
2025-11-25 15:25:40 +0000

    Add auto-create admin command

---
+++ orders/management/commands/create_admin.py
@@ -0,0 +1,22 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    help = "Crée un superuser par défaut si inexistant"
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+
+        username = "admin"
+        email = "admin@example.com"
+        password = "Admin1234!"
+
+        if not User.objects.filter(username=username).exists():
+            User.objects.create_superuser(
+                username=username,
+                email=email,
+                password=password
+            )
+            self.stdout.write(self.style.SUCCESS("Superuser créé : admin / Admin1234!"))
+        else:
+            self.stdout.write(self.style.WARNING("Le superuser existe déjà."))

5240c797bff111ecc2ee888be2fa50e2ce9fb211
Désiré Tongbé <desire@example.com>
2025-12-14 23:45:25 +0000

    fix: allow sourcemap files for whitenoise collectstatic

--- fagni/settings.py
+++ fagni/settings.py
@@ -131,6 +131,7 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+WHITENOISE_SKIP_COMPRESS_EXTENSIONS = ('.map',)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'

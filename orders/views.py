b3c00ba2a9a38c4b251c72cf7bd6613ef993fc7f
Désiré Tongbé <desire@example.com>
2025-12-15 12:54:09 +0000

    fix: remove stray '*' + update driver_hub template

--- orders/views.py
+++ orders/views.py
@@ -1,4 +1,4 @@
-*from decimal import Decimal, InvalidOperation, ROUND_HALF_UP
+from decimal import Decimal, InvalidOperation, ROUND_HALF_UP
 from datetime import datetime, timedelta, time
 from django.template.loader import render_to_string
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger

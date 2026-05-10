0e1fe7ebcd4a3f02055b131824182b822deb2775
Désiré Tongbé <desire@example.com>
2026-05-10 23:22:30 +0200

    fix: remplacer Unfold par Django admin standard

--- bonuses/admin.py
+++ bonuses/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from unfold.admin import ModelAdmin as UnfoldModelAdmin
+from django.contrib.admin import ModelAdmin as UnfoldModelAdmin
 
 # Register your models here.

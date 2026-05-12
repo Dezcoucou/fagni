d8975dccbf9bae66a11d2a5f10948d65ddaebbb5
Désiré Tongbé <desire@example.com>
2026-05-12 11:06:33 +0200

    fix: CORS ajouter fagni-partner.vercel.app

--- fagni/settings.py
+++ fagni/settings.py
@@ -514,5 +514,6 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://localhost:3000",
     "https://fagni-client.vercel.app",
+    "https://fagni-partner.vercel.app",
 ]
 CORS_ALLOW_CREDENTIALS = True

5072a90faae0665198c93eae6972345816e9b72d
Désiré Tongbé <desiretongbe@gmail.com>
2026-09-19 00:38:14 +0200

    fix(cors): allow local OPS development

--- fagni/settings.py
+++ fagni/settings.py
@@ -594,6 +594,7 @@ REST_FRAMEWORK = {
 }
 
 CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
     "https://fagni-client.vercel.app",
     "https://fagni-client-ig3z5rdtk-dezcoucous-projects.vercel.app",
     "https://fagni-driver.vercel.app",
